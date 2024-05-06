18565e001c4d035d198e321b737c7c1ec80ec678
Aravindchwdry <101628815+Aravindchwdry@users.noreply.github.com>
2024-05-06 13:47:23 -0400

    Update twitter_commands.sh

--- twitter_commands.sh
+++ twitter_commands.sh
@@ -1,7 +1,7 @@
 sudo apt-get update
 sudo apt install python3-pip python3-venv python3-wheel
-python3 -m venv airflow-env
-source airflow-env/bin/activate
+python3 -m venv airflow-env #created a virtual environment for installing dependencies in ubuntu instance
+source airflow-env/bin/activate 
 pip install apache-airflow
 sudo pip install pandas 
 sudo pip install s3fs
