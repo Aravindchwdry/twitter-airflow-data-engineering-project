94160c23eabe3eb4d590146cef667c8fa6bb4bde
Aravindchwdry <101628815+Aravindchwdry@users.noreply.github.com>
2024-05-06 13:46:26 -0400

    Update twitter_commands.sh

--- twitter_commands.sh
+++ twitter_commands.sh
@@ -1,6 +1,8 @@
 sudo apt-get update
-sudo apt install python3-pip
-sudo pip install apache-airflow
+sudo apt install python3-pip python3-venv python3-wheel
+python3 -m venv airflow-env
+source airflow-env/bin/activate
+pip install apache-airflow
 sudo pip install pandas 
 sudo pip install s3fs
 sudo pip install tweepy
